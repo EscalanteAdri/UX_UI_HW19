c303c864d1e1011d41176fea452dbed4b33e4b78
EscalanteAdri <135542896+EscalanteAdri@users.noreply.github.com>
2023-11-13 18:52:48 -0500

    added js
    MyWork.html added jquery functionality

--- js/index.js
+++ js/index.js
@@ -1,77 +1,130 @@
-var currentSection = 1;
-var sections;
-
-$('document').ready(function () {
-	setCurrentSection();
-	setSectionIndicator(1);
-	$('.navigation').click(function () {
-		if (this.id == "nav-left") { 
-			if (currentSection != 1) {
-				scrollToSection(currentSection - 1);
-				$('#nav-left').prop('disabled', true);
-			}
-		}
-		else  {
-			if (currentSection+1 <= sections) {
-				scrollToSection(currentSection + 1);
-				$('#nav-right').prop('disabled', true);
-			}
-		}
-	});
-
-	$('.navigation').on('mouseover', function() {
-		if (this.id == 'nav-left') {
-			if (currentSection != 1) $(this).addClass('navigation-hover');
-		} else {
-			if (currentSection+1 <= sections) $(this).addClass('navigation-hover');
-		}
-	}).on('mouseout', function () {
-		$(this).removeClass('navigation-hover');
-	});
-
-	$(window).resize(function() {
-		setCurrentSection();
-		setSectionIndicator(currentSection);
-	});
-
-});
-
-function setCurrentSection() {
-	var carouselWidth = $('.carousel').width();
-	var projectWidth = $('.project').width() + parseInt($('.project').css('margin-right').replace('px',''));
-	var projectsQtt = $('.project').size();
-	var projectsPerSection = carouselWidth / projectWidth;
-	sections = Math.round(projectsQtt / projectsPerSection);
-	var rollLeft = Math.abs(parseInt($('.roll').css('left').replace('px','')));
-	if (rollLeft == 0) currentSection = 1;
-	else {
-		currentSection = Math.round((rollLeft / carouselWidth) + 1);
-	}
-	$('#nav-left').prop('disabled', false);
-	$('#nav-left').removeClass('navigation-hover');
-	$('#nav-right').prop('disabled', false);
-	$('#nav-right').removeClass('navigation-hover');
-}
-
-function scrollToSection(section) {
-	var width = $('.carousel').width() * (Math.abs(currentSection - section));
-	if (section < currentSection) {
-		$('.roll').animate({left: '+='+width}, "slow", setCurrentSection);
-	} else {
-		$('.roll').animate({left: '-='+width}, "slow", setCurrentSection);
-	}
-	setSectionIndicator(section);
-}
-
-function setSectionIndicator(section) {
-	$('.sections').html('');
-	for (var i = 1; i <= sections; i++) {
-		if (i == section) circleClass = 'fa fa-circle';
-		else circleClass = 'fa fa-circle-thin';
-		$('.sections').html($('.sections').html() + '<i class="'+circleClass+' indicator" data-id="'+i+'" aria-hidden="true"></i>');
-	}
-	$('.indicator').click(function() {
-		scrollToSection($(this).attr('data-id'));
-	});
-}
-
+jQuery(document).ready(function() {
+    function scrollbtnshow() {
+      if (
+        $("#nav--tab li.active").prev("li").length > 0 &&
+        $("#nav--tab li.active").next("li").length > 0
+      ) {
+        $(".slide-cntrl").addClass("overall-gradient-float");
+        $(".slide-cntrl").removeClass("left-gradient-float");
+        $(".slide-cntrl").removeClass("right-gradient-float");
+        $(".slide-cntrl-btn.btn-next").css("display", "block");
+        $(".slide-cntrl-btn.btn-prev").css("display", "block");
+      } else if (
+        $("#nav--tab li.active").prev("li").length == 0 &&
+        $("#nav--tab li.active").next("li").length > 0
+      ) {
+        $(".slide-cntrl").removeClass("overall-gradient-float");
+        $(".slide-cntrl").removeClass("left-gradient-float");
+        $(".slide-cntrl").addClass("right-gradient-float");
+        $(".slide-cntrl-btn.btn-prev").css("display", "none");
+      } else {
+        $(".slide-cntrl").removeClass("overall-gradient-float");
+        $(".slide-cntrl").addClass("left-gradient-float");
+        $(".slide-cntrl").removeClass("right-gradient-float");
+        $(".slide-cntrl-btn.btn-next").css("display", "none");
+      }
+    }
+  
+    function tabcontentselect() {
+      var panelId = jQuery(".tab-selection ul li.active").attr("id");
+  
+      jQuery(".tabbed-content").hide();
+      jQuery(".tabbed-content").removeClass("active-tab");
+      jQuery("." + panelId).show(), 500;
+      jQuery("." + panelId).addClass("active-tab");
+    }
+  
+    jQuery(".tab-selection ul li").click(function() {
+      jQuery(this)
+        .siblings()
+        .removeClass("active");
+      jQuery(this).addClass("active");
+  
+      tabcontentselect();
+    });
+  
+    jQuery("#notification-tab").click(function() {
+      jQuery(this)
+        .parent()
+        .parent()
+        .removeClass("left");
+    });
+  
+    jQuery("#nav--tab li").on("click", function() {
+      jQuery("#nav--tab li").removeClass("active");
+      jQuery(this).addClass("active");
+      jQuery("#nav--tab").scrollCenter(".active", 300);
+  
+      scrollbtnshow();
+    });
+  
+    /* Tab buttons */
+  
+    jQuery("#prev").on("click", function() {
+      jQuery(".slide-cntrl-btn.btn-next").css("display", "block");
+      jQuery("#nav--tab li.active:first")
+        .prev()
+        .addClass("active");
+      jQuery("#nav--tab li.active")
+        .next()
+        .removeClass("active");
+      jQuery("#nav--tab").scrollCenter(".active", 300);
+    });
+  
+    jQuery("#next").on("click", function() {
+      jQuery(".slide-cntrl-btn.btn-prev").css("display", "block");
+      jQuery("#nav--tab li.active:last")
+        .next()
+        .addClass("active");
+      jQuery("#nav--tab li.active")
+        .prev()
+        .removeClass("active");
+      jQuery("#nav--tab").scrollCenter(".active", 300);
+    });
+  
+    jQuery(".slide-cntrl-btn").on("click", function(e) {
+      e.preventDefault();
+      scrollbtnshow();
+      tabcontentselect();
+    });
+  
+    /* END Tab buttons */
+  
+    if ($(".nav--tab").prop("scrollWidth") > $(".nav--tab").width()) {
+      $(".slide-cntrl").css("display", "block");
+    } else {
+      $(".slide-cntrl").css("display", "none");
+    }
+  
+    jQuery.fn.scrollCenter = function(elem, speed) {
+      var active = jQuery(this).find(elem);
+      var activeWidth = active.width() / 2;
+  
+      var pos = active.position().left + activeWidth;
+      var currentscroll = jQuery(this).scrollLeft();
+      var divwidth = jQuery(this).width();
+      pos = pos + currentscroll - divwidth / 2;
+  
+      jQuery(this).animate(
+        {
+          scrollLeft: pos
+        },
+        speed == undefined ? 1000 : speed
+      );
+      return this;
+    };
+  
+    jQuery.fn.scrollCenterORI = function(elem, speed) {
+      jQuery(this).animate(
+        {
+          scrollLeft:
+            jQuery(this).scrollLeft() -
+            jQuery(this).offset().left +
+            jQuery(elem).offset().left
+        },
+        speed == undefined ? 1000 : speed
+      );
+      return this;
+    };
+  });
+  
